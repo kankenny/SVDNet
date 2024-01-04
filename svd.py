b04d4dc67f369d3d223d8c4b1f4a841acd301a79
kenny <97374837+kankenny@users.noreply.github.com>
2024-01-04 20:11:30 +0000

    feat: implement single channel and multi channel image compression

--- svd.py
+++ svd.py
@@ -5,6 +5,43 @@ from scipy import linalg
 
 ex_img_path = '/workspaces/workspace/res/cat.jpg'
 
-image = Image.open(ex_img_path)
+img = Image.open(ex_img_path)
+np_img = np.array(img)
 
-image.show()
+
+def compress_sc(a, comp_rate=0.2):
+    """
+    Single-Channel image compression using Singular Value Decomposition
+
+    I: a - matrix-like arr (2d), comp_rate - rate of rank 1 matrices disregarded
+    O: compressed_img - sum of rank 1 matrices 
+    """
+    U, S, V = linalg.svd(a)
+    
+    k = int((1 - comp_rate) * min(a.shape))
+    print(k)
+    
+    U_trunc = U[:, :k]
+    S_trunc = np.diag(S[:k])
+    V_trunc = V[:k, :]
+    
+    compressed_img = U_trunc @ S_trunc @ V_trunc
+    
+    return compressed_img
+
+def compress_mc(a, comp_rate=0.2):
+    """
+    Multi-Channel image compression using Singular Value Decomposition
+
+    I: a - matrix-like arr (3d), comp_rate - rate of rank 1 matrices disregarded
+    O: compressed_img - sum of rank 1 matrices 
+    """
+    R = compress_sc(a[..., 0])
+    G = compress_sc(a[..., 1])
+    B = compress_sc(a[..., 2])
+
+    compressed_img = np.stack([R, G, B], axis=-1)
+    
+    return compressed_img
+
+compress_mc(np_img)
