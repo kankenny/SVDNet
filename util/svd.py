c59c971b1a4edc43d6cfbd5cce63f23e9e6daa0f
kenny <97374837+kankenny@users.noreply.github.com>
2024-01-04 20:59:50 +0000

    refactor: update docs

--- util/svd.py
+++ util/svd.py
@@ -11,10 +11,18 @@ np_img = np.array(img)
 
 def compress_sc(a, comp_rate=0.2):
     """
-    Single-Channel image compression using Singular Value Decomposition
+    Single-Channel image compression using Singular Value Decomposition (SVD)
 
-    I: a - matrix-like arr (2d), comp_rate - rate of rank 1 matrices disregarded
-    O: compressed_img - sum of rank 1 matrices 
+    Args:
+        a (numpy.ndarray): Input image as a 3D tensor (height x width x channels).
+        comp_rate (float, optional): Rate of 'least valuable' rank-1 matrices disregarded (default is 0.2).
+        chan_wise (bool, optional): If True, apply independent SVD on individual channels; otherwise, on the average of the channel-wise pixel values (default is True).
+
+    Returns:
+        numpy.ndarray: Compressed image as a 3D tensor.
+
+    Raises:
+        ValueError: If the input tensor 'a' is not a 3D tensor.
     """
     U, S, V = linalg.svd(a)
     
@@ -28,13 +36,28 @@ def compress_sc(a, comp_rate=0.2):
     
     return compressed_img
 
-def compress_mc(a, comp_rate=0.2):
+def compress_mc(a, comp_rate=0.2, chan_wise=True):
     """
-    Multi-Channel image compression using Singular Value Decomposition
+    Multi-Channel image compression using SVD
 
-    I: a - matrix-like arr (3d), comp_rate - rate of rank 1 matrices disregarded
-    O: compressed_img - sum of rank 1 matrices 
+    Args:
+        a (numpy.ndarray): Input image as a 3D tensor (height x width x channels).
+        comp_rate (float, optional): Rate of 'least valuable' rank-1 matrices disregarded (default is 0.2).
+        chan_wise (bool, optional): If True, apply independent SVD on individual channels; otherwise, on the average of the channel-wise pixel values (default is True).
+
+    Returns:
+        numpy.ndarray: Compressed image as a 3D tensor.
+
+    Raises:
+        ValueError: If the input tensor 'a' is not a 3D tensor.
     """
+    if len(a.shape) != 3:
+        raise ValueError("Input tensor 'a' must be a 3D tensor with shape (height, width, channels).")
+
+    if chan_wise == False:  # Average the corresponding pixel-wise values in the channel dimension
+        avg = np.sum(a, axis=2) / 3
+        a[..., 0:3] = avg[..., None]
+
     R = compress_sc(a[..., 0], comp_rate=comp_rate)
     G = compress_sc(a[..., 1], comp_rate=comp_rate)
     B = compress_sc(a[..., 2], comp_rate=comp_rate)
